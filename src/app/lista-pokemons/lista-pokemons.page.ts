8973b54a35e19f5ea1492c621f62e6a98a3650e5
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-03-08 19:13:13 -0300

    :sparkles: Adicionando searchbar na lista de pokemons

--- src/app/lista-pokemons/lista-pokemons.page.ts
+++ src/app/lista-pokemons/lista-pokemons.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DadosPokemonService } from '../services/dados-pokemon.service';
 import { PokemonClient } from 'pokenode-ts';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-pokemons',
@@ -9,9 +10,9 @@ import { PokemonClient } from 'pokenode-ts';
   styleUrls: ['./lista-pokemons.page.scss'],
 })
 export class ListaPokemonsPage {
-
   pokemonCliente = new PokemonClient();
 
+  pesquisa: string;
   pokemons: any = [];
   pokemonsPage: any = [];
   private readonly offset: number = 20;
@@ -21,13 +22,14 @@ export class ListaPokemonsPage {
 
   constructor(
     private route: Router,
-    private dadosPokemonService: DadosPokemonService
+    private dadosPokemonService: DadosPokemonService,
+    public toastController: ToastController
   ) {
     this.getPokemons();
   }
 
   async getPokemons() {
-    for (let i = this.index+1; i <= this.offset+this.index; i++) {
+    for (let i = this.index + 1; i <= this.offset + this.index; i++) {
       console.log(i);
       await this.pokemonCliente
         .getPokemonById(i)
@@ -53,5 +55,25 @@ export class ListaPokemonsPage {
     if (this.pokemonsPage.length === this.totalPokemons) {
       event.target.disabled = true;
     }
+  }
+
+  async buscarPokemon(event) {
+    if (this.pesquisa && this.pesquisa.trim() !== '') {
+      await this.pokemonCliente
+        .getPokemonByName(this.pesquisa)
+        .then((data) => {
+          this.exibirPokemon(data);
+        })
+        .catch((error) => this.pokemonToast());
+    }
+  }
+
+  async pokemonToast() {
+    const toast = await this.toastController.create({
+      message: 'Pokemon nao encontrado',
+      color: 'danger',
+      duration: 2000
+    });
+    toast.present();
   }
 }
