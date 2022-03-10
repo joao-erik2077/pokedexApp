269ed07e561a0845199e32a3ebd63b4cd1142daf
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-03-09 21:16:11 -0300

    :lipstick: Melhorias na tela de dados sobre pokemon

--- src/app/pokemon/pokemon.page.ts
+++ src/app/pokemon/pokemon.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../models/Pokemon.model';
 import { DadosPokemonService } from '../services/dados-pokemon.service';
+import { MoveClient } from 'pokenode-ts';
 
 @Component({
   selector: 'app-pokemon',
@@ -8,13 +9,27 @@ import { DadosPokemonService } from '../services/dados-pokemon.service';
   styleUrls: ['./pokemon.page.scss'],
 })
 export class PokemonPage implements OnInit {
-
   pokemon: Pokemon;
+  actualSegment: string = 'dados';
+  moveClient = new MoveClient();
 
-  constructor(private dadosPokemonService: DadosPokemonService) { }
+  constructor(
+    private dadosPokemonService: DadosPokemonService,
+  ) {}
 
   ngOnInit() {
     this.pokemon = this.dadosPokemonService.pegarDados('pokemon');
   }
 
+  segmentChange(event) {
+    if (event.target.value === 'dados') {
+      this.actualSegment = 'dados';
+    } else if (event.target.value === 'moves') {
+      this.actualSegment = 'moves';
+    }
+  }
+
+  getColor(tipo): string {
+    return `type-${tipo.type.name}`;
+  }
 }
