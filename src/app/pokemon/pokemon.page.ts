db16165494369e165a23d6d0e7ba0102115b4d11
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-03-15 20:16:13 -0300

    :sparkles: Adicionado relação de tipos (ataque e defesa)

--- src/app/pokemon/pokemon.page.ts
+++ src/app/pokemon/pokemon.page.ts
@@ -20,6 +20,98 @@ export class PokemonPage implements OnInit {
   private readonly offset: number = 10;
   private index: number = 0;
   private totalMoves: number;
+  private typeRelations: any = [
+    {
+      name: 'bug',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'dark',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'dragon',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'electric',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'fairy',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'fighting',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'fire',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'flying',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'ghost',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'grass',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'ground',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'ice',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'normal',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'poison',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'psychic',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'rock',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'steel',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+    {
+      name: 'water',
+      valueATK: 1,
+      valueDEF: 1,
+    },
+  ];
 
   slideOpts = {
     initialSlide: 0,
@@ -31,6 +123,7 @@ export class PokemonPage implements OnInit {
   async ngOnInit() {
     this.id = this.dadosPokemonService.pegarDados('pokemon');
     await this.getPokemon(this.id);
+    this.getTypeRelation();
     this.totalMoves = this.pokemon.moves.length;
     this.getMoves();
   }
@@ -113,5 +206,80 @@ export class PokemonPage implements OnInit {
         break;
     }
     return statFormatted;
+  }
+
+  async getTypeRelation() {
+    let typeRelation;
+    let type1, type2;
+
+    type1 = this.pokemon.types[0].type.name;
+    if (this.pokemon.types[1]) type2 = this.pokemon.types[1].type.name;
+
+    await this.pokemonClient
+      .getTypeByName(type1)
+      .then((data) => {
+        typeRelation = data.damage_relations;
+      })
+      .catch((error) => console.error(error));
+
+    this.typeRelationsSet(typeRelation);
+
+    if (type2) {
+      await this.pokemonClient
+        .getTypeByName(type2)
+        .then((data) => {
+          typeRelation = data.damage_relations;
+        })
+        .catch((error) => console.error(error));
+      this.typeRelationsSet(typeRelation);
+    }
+  }
+
+  typeRelationsSet(typeRelation) {
+    for (let i = 0; i < this.typeRelations.length; i++) {
+      for (let j = 0; j < typeRelation.double_damage_from.length; j++) {
+        if (
+          typeRelation.double_damage_from[j].name === this.typeRelations[i].name
+        )
+          this.typeRelations[i].valueDEF *= 2;
+      }
+      for (let j = 0; j < typeRelation.double_damage_to.length; j++) {
+        if (
+          typeRelation.double_damage_to[j].name === this.typeRelations[i].name
+        )
+          this.typeRelations[i].valueATK *= 2;
+      }
+      for (let j = 0; j < typeRelation.half_damage_from.length; j++) {
+        if (
+          typeRelation.half_damage_from[j].name === this.typeRelations[i].name
+        )
+          this.typeRelations[i].valueDEF *= 0.5;
+      }
+      for (let j = 0; j < typeRelation.half_damage_to.length; j++) {
+        if (typeRelation.half_damage_to[j].name === this.typeRelations[i].name)
+          this.typeRelations[i].valueATK *= 0.5;
+      }
+      for (let j = 0; j < typeRelation.no_damage_from.length; j++) {
+        if (typeRelation.no_damage_from[j].name === this.typeRelations[i].name)
+          this.typeRelations[i].valueDEF = 0;
+      }
+      for (let j = 0; j < typeRelation.no_damage_to.length; j++) {
+        if (typeRelation.no_damage_to[j].name === this.typeRelations[i].name)
+          this.typeRelations[i].valueATK = 0;
+      }
+    }
+  }
+
+  getTypeRelationColorATK(value: number): string {
+    if (value === 0) return 'dark';
+    if (value === 1) return 'medium';
+    if (value <= 0.5) return 'danger';
+    if (value >= 2) return 'success';
+  }
+  getTypeRelationColorDEF(value: number): string {
+    if (value === 0) return 'dark';
+    if (value === 1) return 'medium';
+    if (value <= 0.5) return 'success';
+    if (value >= 2) return 'danger';
   }
 }
